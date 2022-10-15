a1ad6707aa94a80769431ab19a9097471d031ead
MalvadoPerro <diman_1205@mail.ru>
2022-10-15 11:31:51 +0300

    Решена задача 1.1 и 1.2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //1.1
+        System.out.println("Version 1.1");
+        //1.2
+        System.out.println("Version 1.2");
+    }
+}
